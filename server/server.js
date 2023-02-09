e572ac0b525e428f1b4fd153f7004b2fcefb03a5
Nicolas <niceckell@gmail.com>
2023-02-09 01:36:50 -0300

    start script for heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const DIST_DIR = path.join(__dirname, "/build");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+app.use(express.static(DIST_DIR));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(publicPath, "index.html"));
+	res.sendFile(path.join(HTML_FILE, "index.html"));
 });
 app.listen(port, () => {
 	console.log("Server is up!");
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import Root from './root';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import Root from "./root";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Root />
+		ASDASD
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
