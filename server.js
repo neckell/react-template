11540d811f79a7403cdf7bcaa6ab817590bbc9f3
Nicolas <niceckell@gmail.com>
2023-02-09 01:39:00 -0300

    start script for heroku

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const DIST_DIR = path.join(__dirname, "/build");
-const HTML_FILE = path.join(DIST_DIR, "index.html");
 const port = process.env.PORT || 3000;
 app.use(express.static(DIST_DIR));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(HTML_FILE, "index.html"));
+	res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 app.listen(port, () => {
 	console.log("Server is up!");
