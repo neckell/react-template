7d19b28bfeef3b4563ce3607afeaafa9187d845e
Nicolas Eckell <niceckell@gmail.com>
2025-02-06 05:11:23 -0300

    insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import * as React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Layout from "./app/layout/Layout"
 import { store } from "./app/redux/config/store"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      <SpeedInsights />
       <Layout />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
